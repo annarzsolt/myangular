9e81e1e21042473e4319650146f24688d27ac785
DESKTOP-A0BT22A\annarzsolt <92annar92@gmail.com>
2017-12-04 23:28:42 +0100

    Add new record

--- src/app/heroes/mock-heroes.ts
+++ src/app/heroes/mock-heroes.ts
@@ -10,5 +10,6 @@ export const mockHeroes: Hero[] = [
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 20, name: 'Magma' },
+  { id: 21, name: 'Tornado' }
 ];
